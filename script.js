6e87ba99f2cf4300f4368a71035b24254ee790c8
s0604798 <juri.wende@student.htw-berlin.de>
2026-07-24 12:47:18 +0200

    blib2

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-import fs from 'fs';
-const fs = require('fs');
-
 window.onload = function () {
     getImages();
     fillGallery();
 }
 
 const gallery = {
-    elem : getElementById("myGallery"),
+    element: document.getElementById("myGallery"),
+    source: 'imgs/',
     images: [],
-
+    length: 35,
 }
 
+let images = new Array(gallery.length);
 function getImages() {
-    let images = fs.readdirSync('./imgs');
-    console.log(images);
+
+    for (let i = 1; i < gallery.length; i++) {
+        let nums = [0, 0, 0];
+        let arr = Array.from(String(i, Number));
+        arr.reverse();
+
+        for (let i = 0; i < 3; i++) { nums[i] = i < arr.length ? arr[i] : 0; }
+        nums.reverse();
+
+        images[i] = nums.toString().replace(/\D/g,"") + ".jpg";
+    }
+
+    gallery.images = images;
 }
 
 function fillGallery() {
+    for (let i = 1; i < gallery.length; i++) {
+        let img = document.createElement("img");
+        let src = "imgs/";
+        img.src = src + images[i];
+        gallery.element.appendChild(img);
+    }
 }
