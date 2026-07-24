b2acde8e04c383d36a0cb7f20ad9e5a5e53aea55
s0604798 <juri.wende@student.htw-berlin.de>
2026-07-24 18:28:22 +0200

    gallery

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const page = document.getElementById("body");
 window.onload = function () {
     getImages();
     fillGallery();
@@ -5,14 +6,14 @@ window.onload = function () {
 
 const gallery = {
     element: document.getElementById("myGallery"),
-    source: 'imgs/',
+    source: "imgs/webp/",
+    suffix: ".webp",
     images: [],
     length: 35,
 }
 
 let images = new Array(gallery.length);
 function getImages() {
-
     for (let i = 1; i < gallery.length; i++) {
         let nums = [0, 0, 0];
         let arr = Array.from(String(i, Number));
@@ -21,17 +22,53 @@ function getImages() {
         for (let i = 0; i < 3; i++) { nums[i] = i < arr.length ? arr[i] : 0; }
         nums.reverse();
 
-        images[i] = nums.toString().replace(/\D/g,"") + ".jpg";
+        images[i] = nums.toString().replace(/\D/g,"");
     }
-
     gallery.images = images;
 }
 
 function fillGallery() {
     for (let i = 1; i < gallery.length; i++) {
-        let img = document.createElement("img");
-        let src = "imgs/";
-        img.src = src + images[i];
-        gallery.element.appendChild(img);
+        let id = images[i];
+
+        const container = document.createElement("div");
+        const overlay = document.createElement("div");
+        const button = document.createElement("button");
+        const popup = document.createElement("img");
+        const item = document.createElement("img");
+
+        container.setAttribute("class", "container");
+        overlay.setAttribute("class", "overlay");
+        button.setAttribute("class", "popBtn");
+        popup.setAttribute("class", "popUp");
+        item.setAttribute("class", "item");
+
+        container.id = "container-" + id;
+        overlay.id = "overlay-" + id;
+        button.id = "button-" + id;
+        popup.id = "popup-" + id;
+        item.id = "item-" + id;
+
+        item.setAttribute("loading", "lazy");
+        item.src = gallery.source + id + gallery.suffix;
+        container.appendChild(item);
+
+        popup.setAttribute("popover", "auto");
+        popup.src = gallery.source + id + gallery.suffix;
+        page.appendChild(popup);
+
+        button.innerText = id;
+        button.setAttribute("popovertarget", popup.id);
+        overlay.appendChild(button);
+        container.appendChild(overlay);
+
+        container.addEventListener("mouseover", function () { display(overlay) })
+        container.addEventListener("mouseleave", function () { hide(overlay) });
+
+        gallery.element.appendChild(container);
     }
 }
+
+function display(elem) { elem.setAttribute("style", "display: block;"); }
+
+function hide(elem) { elem.setAttribute("style", "display: none;"); }
